07ef677e1ce543201bd0da4d817de53a1b81dd85
Martin Farrera <farreralara.martin@gmail.com>
2022-04-04 19:27:49 -0500

    Second Commit

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,7 +4,7 @@ import subprocess
 MESSAGE_COLOR = "\x1b[34m"
 RESET_ALL = "\x1b[0m"
 
-print(f"{MESSAGE_COLOR}Almost done!")
+print(f"{MESSAGE_COLOR}Almost done!!")
 print(f"Initializing a git repository...{RESET_ALL}")
 
 subprocess.call(['git', 'init'])
